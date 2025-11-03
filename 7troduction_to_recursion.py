f81a2c88abc9ac0f8eb97be132792d4e0035dcd2
Solanki Mahesh <Solanki777>
2025-11-03 21:40:18 +0530

    DSA

--- 7troduction_to_recursion.py
+++ 7troduction_to_recursion.py
@@ -1,8 +1,20 @@
 # Python is smart ? because if run any recursing having infinite calling than et some point arround 1000 it stops calling and thorugh an error that RecursionError: maximum recursion depth exceeded let example:
 
-def hello(i):
-    print(i)
-    hello(i+1)
-i=1
-hello(i)
+# def hello(i):
+#     print(i)
+#     hello(i+1)
+# i=1
+# hello(i)
 
+# example 1: print Anirudh 4 time
+
+
+i=0
+def ani():
+    global i
+    if i==4:
+        return
+    print("Anirudh")
+    i+=1
+    ani()
+ani()
