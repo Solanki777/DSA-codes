f6d588d1c7ed64f99691ceaee67dcd8f84b442ea
Solanki Mahesh <Solanki777>
2025-11-05 19:23:31 +0530

    DSA

---
+++ 9function_recursion.py
@@ -0,0 +1,21 @@
+# Here we do some promblem like sum of 1 to N numbers using recursion
+# mehtod used by code and debug
+
+# parameterized recursions:where printitng is done within a function
+# def fun(sum,n,i):
+#     if i==n+1:
+#         print(sum)
+#         return
+#     fun(sum+i,n,i+1)
+# fun(0,10,0)
+
+# function recursion: where the recusrsive funtion returns the answer and then it will prints . Here the printing is not done within the function
+# my mehtod:
+# def rec(n):
+#     if n==0:
+#         return 0
+#     else:
+#         return n+rec(n-1)
+    
+# sum=rec(10)
+# print(sum)
