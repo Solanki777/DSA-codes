0c01b7516c13e75f67912a30e91b0092d29511d6
Solanki Mahesh <Solanki777>
2025-12-22 12:42:57 +0530

    DSA

--- 50introduction_to_linked_list.py
+++ 50introduction_to_linked_list.py
@@ -50,6 +50,49 @@ class Sinlgy:
                 curr=curr.next
             curr.next=new_node
 
+    def insert_at(self,data,position):
+        new_node=Node(data)
+        new_node.next=None
+        curr=self.head
+        pre_node=None
+        count=0
+
+        if position==0:
+            new_node.next=self.head
+            self.head=new_node
+        else:
+            while count<position and curr is not None:
+                pre_node=curr
+                curr=curr.next
+                count+=1
+            pre_node.next=new_node
+            new_node.next=curr
+
+    def delete(self,data):
+        temp=self.head
+        # case1:empty list
+        if self.head==None:
+            print("empty SSL")
+            return
+        
+        # case2:head  to be deleted
+        if temp.value==data:
+            self.head=temp.next
+            return
+        
+        # case3 : search the node
+        prev=None
+        while temp is not None:
+            if temp.value==data:
+                prev.next=temp.next
+                return
+            prev=temp
+            temp=temp.next
+
+        # case 4: not not found
+        print("node not found")
+        
+        
     def traverse(self):
         if self.head is None:
             print("SSL is empty")
@@ -62,14 +105,22 @@ class Sinlgy:
             print("None")
 
 ssl = Sinlgy()
-ssl.traverse()          
+ssl.traverse()
 ssl.append(10)
-ssl.traverse()         
+ssl.traverse()
 ssl.append(20)
 ssl.append(30)
 ssl.append(40)
 ssl.traverse()
         
+# | Operation          | Time Complexity | Space Complexity |
+# | ------------------ | --------------- | ---------------- |
+# | Append             | `O(n)`          | `O(1)`           |
+# | Insert at head     | `O(1)`          | `O(1)`           |
+# | Insert at position | `O(n)`          | `O(1)`           |
+# | Delete by value    | `O(n)`          | `O(1)`           |
+# | Traverse           | `O(n)`          | `O(1)`           |
+# | Search             | `O(n)`          | `O(1)`           |
 
 
 
