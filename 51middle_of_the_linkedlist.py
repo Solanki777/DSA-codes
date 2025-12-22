cf39b9b678ea512079b9a7a29eb33810a4a0dc6e
Solanki Mahesh <Solanki777>
2025-12-22 17:36:12 +0530

    DSA

---
+++ 51middle_of_the_linkedlist.py
@@ -0,0 +1,137 @@
+import math
+class Node:
+    def __init__(self,val):
+        self.value=val
+        self.next=None
+
+class Sinlgy:
+    def __init__(self):
+        self.head=None
+
+    def append(self,value):
+        new_node=Node(value)
+        curr=self.head
+
+        # if linked list is empty
+        if self.head==None:
+            self.head=new_node
+            return
+        
+        else:
+            while curr.next!=None:
+                curr=curr.next
+            curr.next=new_node
+
+    def travers(self):
+        curr=self.head
+        if self.head==None:
+            print("linked list is empty")
+            return
+        else:
+            while curr!=None:
+                print(curr.value,end=" ")
+                curr=curr.next
+
+            print("None")
+
+    def insertat(self,position,data):
+        new_node=Node(data)
+        curr=self.head
+        prev_node=None
+        count=0
+
+        if position==0:
+            new_node.next=self.head
+            self.head=new_node
+            return
+
+        else:
+            while curr!=None and count<position:
+                prev_node=curr
+                curr=curr.next
+                count+=1
+
+            if count!=position:
+                print("position not found")
+                return
+            
+            prev_node.next=new_node
+            new_node.next=curr
+ 
+
+
+    def delateat(self,data):
+
+        # linkedlist empty
+        if self.head==None:
+            print("linkedlist is empty")
+            return
+        
+        # first node
+        if self.head.value==data:
+            self.head=self.head.next
+            return
+        
+        curr=self.head
+        pre_node=None
+        found=False
+
+        while curr!=None:
+            if curr.value==data:
+                found=True
+                break
+            else:
+                pre_node=curr
+                curr=curr.next
+
+        if found:
+            pre_node.next=curr.next
+        
+        else:
+            print("not found")
+    #   my
+    # def middle(self):
+    #     if self.head.next==None:
+    #         return self.head.value
+    #     count=0
+    #     curr=self.head
+    #     while curr!=None:
+    #         curr=curr.next
+    #         count+=1
+
+    #     mid=count//2
+
+    #     curr=self.head
+    #     count=0
+    #     while curr!=None and count<mid:
+    #         curr=curr.next
+    #         count+=1
+    #     return curr.value
+
+    # optimal solution:tortoise-hare approach
+    def middle(self):
+        slow=self.head
+        fast=self.head
+        while fast!=None and fast.next!=None:
+            slow=slow.next
+            fast=fast.next.next
+        return slow.value
+            
+ssl=Sinlgy()
+ssl.append(10)
+ssl.append(20)
+ssl.append(40)
+ssl.insertat(0,1)
+ssl.insertat(3,30)
+ssl.travers()
+ssl.delateat(1)
+ssl.delateat(30)
+ssl.travers()
+
+# prob:here we have to find the middle element of the linkedlist
+
+
+ans=ssl.middle()
+print(ans)   
+
+# time complexity is for my method is O(N) and space complexity is O(1)
