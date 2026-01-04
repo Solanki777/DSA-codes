4fe945615895b7ff73e76741155d2c098c246e52
Solanki Mahesh <Solanki777>
2026-01-04 10:52:06 +0530

    DSA

---
+++ 64introduction_to_bit_manipulation.py
@@ -0,0 +1,12 @@
+# 1.Binary Number Conversion:
+
+#    9->1001 13->1101 so this is conversion of decimal to binary
+     1001->9 1101-> vice versa also
+
+# 2.Diffrenet types of operators:AND,OR,XOR,NOT,SHIFT
+# 3.Swapping two numbers
+# 4. Check if the ith bit is set or not
+# 5. Extract the ith bit
+# 6. Set the ith bit 
+# 7. Clear the ith bit
+# 8. Toggle the ith bit
