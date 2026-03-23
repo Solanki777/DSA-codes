ac74a6f4d7d290cd04ae0c6ddc29bcc40df2bceb
Solanki Mahesh <Solanki777>
2026-03-23 22:53:02 +0530

    DSA

--- 108intro_to_binarytree.py
+++ 108intro_to_binarytree.py
@@ -1,17 +1,17 @@
-1. what is binary tree ?
-Ans:A binary tree is a hierarchical, non-linear data structure where each node has at most two children — referred to as the left child and right child. The root is the topmost node, while leaf nodes have no children. Each node typically stores data and pointers/references to its children.
+# 1. what is binary tree ?
+# Ans:A binary tree is a hierarchical, non-linear data structure where each node has at most two children — referred to as the left child and right child. The root is the topmost node, while leaf nodes have no children. Each node typically stores data and pointers/references to its children.
 
-2. In a binary tree, the root is the topmost node, and each node has at most two children, referred to as the left child and right child. The subtree is the tree formed by the node and all its descendants. The path in a binary tree is a sequence of nodes connected by edges from one node to another. A leaf node is a node that does not have any children, and an internal node is a node that has at least one child. The depth of a node is the number of edges in the path from the root to that node, and the height of a binary tree is the number of edges on the longest path from the root to a leaf node.
-ancestors means parent , parent's parent .... . 
+# 2. In a binary tree, the root is the topmost node, and each node has at most two children, referred to as the left child and right child. The subtree is the tree formed by the node and all its descendants. The path in a binary tree is a sequence of nodes connected by edges from one node to another. A leaf node is a node that does not have any children, and an internal node is a node that has at least one child. The depth of a node is the number of edges in the path from the root to that node, and the height of a binary tree is the number of edges on the longest path from the root to a leaf node.
+# ancestors means parent , parent's parent .... . 
 
-3. Types of Binary tree 
+# 3. Types of Binary tree 
 
-i. Full binary tree : 2 or 0 childrean only .
+# i. Full binary tree : 2 or 0 childrean only .
 
-ii.Complete binary tree : All the levels are full execpt last level(leafnode level). In last level the child are filled from left to right . Commonly used in heap. 
+# ii.Complete binary tree : All the levels are full execpt last level(leafnode level). In last level the child are filled from left to right . Commonly used in heap. 
 
-iii.Perfect binary tree : all node has 2 child and all the leafnodes are at the same level.
+# iii.Perfect binary tree : all node has 2 child and all the leafnodes are at the same level.
 
-iv.Balancec binary tree : for all node check the hight diffrence for right and left subtree must less than or equal to 1 or maximum 1.
+# iv.Balancec binary tree : for all node check the hight diffrence for right and left subtree must less than or equal to 1 or maximum 1.
 
-v. degenerate binary tree : every node has one child execpt lead node 
+# v. degenerate binary tree : every node has one child execpt lead node 
