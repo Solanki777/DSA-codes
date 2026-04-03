6530d29d1334fffc525bf83c30168c18a7a6940c
Solanki Mahesh <Solanki777>
2026-04-03 23:25:27 +0530

    DSA

--- 126distance to near0.py
+++ 126distance to near0.py
@@ -35,5 +35,37 @@ class Solution:
                     mat[u][v] = bfs(u, v)
 
         return mat
-    
+
 # inseted of 1 take 0 and travers thorugh 0 which make it easy 
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+
+        row=len(mat)
+        col=len(mat[0])
+
+        ans=[[0 for _ in range(col)] for _ in range(row)]
+        visited=[[0 for _ in range(col)] for _ in range(row)]
+
+        queue=deque()
+
+        for i in range(row):
+            for j in range(col):
+                if mat[i][j]==0:
+                    queue.append((i,j,0))
+                    visited[i][j]=1
+        
+        while queue:
+            r,c,d=queue.popleft()
+            ans[r][c]=d
+
+            for dx,dy in [[-1,0],[0,-1],[1,0],[0,1]]:
+                new_r=dx+r
+                new_c=dy+c
+                if 0<=new_r<row and 0<=new_c<col and visited[new_r][new_c]==0:
+                    visited[new_r][new_c]=1
+                    queue.append((new_r,new_c,d+1))
+        return ans
+
+# time complexity and space complexity is O(m*n) 
+
+        
