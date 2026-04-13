0cdd48839395d88f0ec59655657377b0ada0b68f
Solanki777 <solankimaheshkhash7@gmail.com>
2026-04-14 00:01:30 +0530

    DSA

--- 147.min_effort.py
+++ 147.min_effort.py
@@ -1,3 +1,9 @@
+
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        row=len(heights)
+        col=len(heights[0])
+        efforts=[[float('inf')*col] for _ in range(row)]
+        
+
         
