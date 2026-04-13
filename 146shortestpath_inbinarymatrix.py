d22e9e5485f57e76adcae7729d8d173859c962df
Solanki777 <solankimaheshkhash7@gmail.com>
2026-04-13 23:21:10 +0530

    DSA

--- 146shortestpath_inbinarymatrix.py
+++ 146shortestpath_inbinarymatrix.py
@@ -1,3 +1,44 @@
+import heapq
+
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        row=len(grid)
+        col=len(grid[0])
+        distance=[[float('inf')] * col for _ in range(row)]
+
+        if grid[0][0]==1:
+            return -1
         
+
+        
+        queue=[([0,0],1)]
+        distance[0][0]=1
+
+        while queue:
+            curr_node,curr_dist=heapq.heappop(queue)
+
+            i,j=curr_node
+
+            if i ==row-1 and j==col-1:
+                return curr_dist
+            
+            neb=[[-1,0],[0,-1],[1,0],[0,1],[1,1],[-1,-1],[-1,1],[1,-1]]
+            for neg in neb:
+                new_i,new_j=neg[0]+i,neg[1]+j
+
+                if  0<=new_i<row and 0<=new_j<col and grid[new_i][new_j]==0 :
+
+                    
+                    new_dist=curr_dist+1
+
+                    if new_dist<distance[new_i][new_j]:
+                        distance[new_i][new_j]=new_dist
+                        heapq.heappush(queue,([new_i,new_j],new_dist))
+        
+        if distance[row-1][col-1]==float('inf'):
+            return -1
+        
+        return distance[row-1][col-1]
+    
+# since here we apply sorting so time complexity is O(m*n *log m*n) and space complexity is o(m*n)
+# optimal solution 
