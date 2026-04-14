b6216817a7b3c7539699710c0d873cc33d7fcd69
Solanki777 <solankimaheshkhash7@gmail.com>
2026-04-14 11:44:11 +0530

    DSA

--- 149mini_multiplication.py
+++ 149mini_multiplication.py
@@ -1,8 +1,30 @@
 #User function Template for python3
 
 from typing import List
- 
-class Solution:
-    
+from collections import deque
+class Solution:    
     def minimumMultiplications(self, arr : List[int], start : int, end : int) -> int:
-        # code here
+        MOD=100000
+        distance=[float('inf')]*MOD
+        ans=float('inf')
+        queue=deque()
+        queue.append((start,0))
+        distance[0]=0
+
+        while queue:
+            node,step=queue.popleft()
+
+            if node==end:
+                return step
+
+            for neb in arr:
+                new_mult=(node*neb)%MOD
+                new_step=step+1
+                if distance[new_mult]>new_step:
+                    queue.append((new_mult,new_step))
+                    distance[new_mult]=new_step
+        return -1
+
+# Time complexity is O(10000 * len(arr)) and space complexity is Space Complexity=O(100000)
+
+        
