3760739ff13c7d3574eccf20e5464b9a39b5b558
Solanki777 <solankimaheshkhash7@gmail.com>
2026-04-14 11:21:04 +0530

    DSA

---
+++ 149mini_multiplication.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+
+from typing import List
+ 
+class Solution:
+    
+    def minimumMultiplications(self, arr : List[int], start : int, end : int) -> int:
+        # code here
