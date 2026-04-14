3f336540b0276b9b8d56719eaa44a2b67c686e28
Solanki777 <solankimaheshkhash7@gmail.com>
2026-04-14 11:15:37 +0530

    DSA

--- 148cheapestFlights.py
+++ 148cheapestFlights.py
@@ -1,3 +1,26 @@
+from collections import deque
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         
+        adjlist=[[] for _ in range(n)]
+
+        for u,v,d in flights:
+            adjlist[u].append((v,d))
+        
+        distanc=[float('inf')]*n
+        queue=deque()
+        queue.append((src,0,0))
+
+        while queue :
+            node,stop,cost=queue.popleft()
+
+            if stop>k:
+                break
+
+            for neb,dis in adjlist[node]:
+                if distanc[neb]>cost+dis:
+                    queue.append((neb,stop+1,cost+dis))
+                    distanc[neb]=cost+dis
+        return -1 if distanc[dst]==float('inf') else distanc[dst]
+
+# time complexity is O(K*E) and space complexity is O(V+E)
