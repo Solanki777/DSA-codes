a4d7026d2362ab3dd8977c83297651671cf9e90c
Solanki777 <solankimaheshkhash7@gmail.com>
2026-04-17 19:11:08 +0530

    DSA

--- leetcode_strike/151bellman-ford.py
+++ leetcode_strike/151bellman-ford.py
@@ -1,4 +1,6 @@
+# Here the node flow his distance all negbour so get the minimum distance 
 
+# 1. if thre is n nodes than the iteration to get all nodes distance is n-1 so we have to do n-1 iteration
 class Solution:
     def bellmanFord(self, V, edges, src):
         #code here
