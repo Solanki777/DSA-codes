ee8fe3a76dbc0d266cb8b1ddaf1de8996b4322cf
Solanki777 <solankimaheshkhash7@gmail.com>
2026-04-18 23:33:24 +0530

    DSA

--- 153findthecitywithsmallestdistance.py
+++ 153findthecitywithsmallestdistance.py
@@ -29,3 +29,56 @@ class Solution:
         return city
 
 # Time complexity is O(n^3) and space complexity is O(n^2)
+
+# optimal solution
+import heapq
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+
+        adj=[[] for _ in range(n)]
+
+        for u,v,w in edges:
+            adj[u].append((v,w))
+            adj[v].append((u,w))
+        
+        
+        def dijkastra(src):
+            dist=[float('inf')]*n
+            dist[src]=0
+
+            heap=[(0,src)]
+            count=0
+
+            while heap:
+                d,node=heapq.heappop(heap)
+            
+                if d>dist[node]:
+                    continue
+
+                for neb,w in adj[node]:
+                    new_dist=d+w
+                    if new_dist<dist[neb]:
+                        heapq.heappush(heap,(new_dist,neb))
+                        dist[neb]=new_dist
+            
+            for i in range(n):
+                if dist[i]<=distanceThreshold:
+                    count+=1
+            return count
+
+
+        city=0
+        conn=n
+        for i in range(n):
+            count=dijkastra(i)
+            if count<=conn:
+                city=i
+                conn=count
+        return city
+
+        
+        
+
+
+# Time complexity is (n*E log V) and space complexity is O(V+E)
+        
