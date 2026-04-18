3496cdafb508b02fabbd7ffca4896c3520ceab81
Solanki777 <solankimaheshkhash7@gmail.com>
2026-04-18 23:47:16 +0530

    MST

---
+++ 154minimum_spanning_tree.py
@@ -0,0 +1,9 @@
+# 1. What is Spanning tree ?
+# spanning tree is a tree having n node and n-1 edges and also all nodes are reachable with each other
+    
+# 2. minimum spanning tree[mst]:
+# mst is spanning tree having minimum total wieght from the all combination of mst.
+
+# thre are two ways of finding mst:
+# 1. Prim's Algorithm
+# 2. Kruskal's Algorithm
