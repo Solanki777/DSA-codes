b29b5e2def6009e43eaa3b8b5fea505b5bd453f7
Solanki777 <solankimaheshkhash7@gmail.com>
2026-04-21 11:25:48 +0530

    Krushkal's Algorithm

--- 154minimum_spanning_tree.py
+++ 154minimum_spanning_tree.py
@@ -43,3 +43,58 @@ class Solution:
                     
 # Time complexity is O(E log V) and space complexity is O(V+E)
 
+# 2. Krushkal's algorithm
+
+class Disjoint:
+    def __init__(self,n):
+        self.size=[1]*(n+1)
+        self.parent=[i for i in range(n+1)]
+    
+    def ultiparent(self,node):
+
+        if self.parent[node]==node:
+            return node
+        
+        self.parent[node]=self.ultiparent(self.parent[node])
+
+        return self.parent[node]
+    
+    def union(self,u,v):
+        pu=self.ultiparent(u)
+        pv=self.ultiparent(v)
+
+        if pu==pv:
+            return False
+        
+        if self.size[pu]<self.size[pv]:
+            self.parent[pu]=pv
+            self.size[pv]+=self.size[pu]
+        
+        else:
+            self.parent[pv]=pu
+            self.size[pu]+=self.size[pv]
+        
+        
+        return True
+
+
+class Solution:
+    def spanningTree(self, V, edges):
+        edge=[]
+        for u,v,w in edges:
+            edge.append((w,u,v))
+        
+        edge.sort()
+
+        mst_weight=0
+        dst=Disjoint(V)
+        for w,u,v in edge:
+            if dst.union(u,v):
+                mst_weight+=w
+        
+        return mst_weight
+
+
+
+
+# Time complexity is O(ElogE) and space complexity is O(V+E)
