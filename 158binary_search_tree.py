bdc1a85de56af3c53e9fc7d9f5057c8c59b2c533
Solanki777 <solankimaheshkhash7@gmail.com>
2026-04-22 10:00:51 +0530

    intro to BST

---
+++ 158binary_search_tree.py
@@ -0,0 +1,5 @@
+# what is binary search tree:
+# it is type of binary tree where childrea followes the valued based place like root is 9 then left childer is smaller than root and right child is higher than root. if there is duplicate it plcaed in left side 
+
+# Why binary search tree 
+# for if we have to find something in binary tree in worst case time complexity is O(N) where we have to traverse all the binary tree. But in binary search tree the time complexity is O(logbase2 N)
