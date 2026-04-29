94ef8e1250999ba714c72f1401cdffbd4298948a
Solanki777 <solankimaheshkhash7@gmail.com>
2026-04-29 20:31:28 +0530

    intro to heap

--- 169introtoheap.py
+++ 169introtoheap.py
@@ -6,11 +6,13 @@
 
 # max heap is special type of binary tree where the parent is grater or equal to its childrean
 
+
 #         50
 #        /  \
 #      30    40
 #     / \   /
 #   10 20 35
+
 
 # in heap : the node values are stored in like this order 
 
@@ -19,11 +21,13 @@
 
 # in vice versa in min heap  parent node is less than or equal to its childrean it uses almost complete binary tree because if you what to add node here then it add from left to right in the tree 
 
+
 #         10
 #        /  \
 #      20    15
 #     / \   /
 #   30 40 25
+
 
 # why max heap used for almost complete binary tree because it's easy to calculate this things'
 
@@ -37,4 +41,4 @@
 
 # the leaf node index goes from n/2 to n-1 in 0 based indexing almost complete binary tree
 
-# internal nodes = 0 to n/2-1
+# internal nodes = 0 to n/2-1    
