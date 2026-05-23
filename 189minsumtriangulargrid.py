1e8797e6a83eb247dec5123aba00c397d27d2671
Solanki777 <solankimaheshkhash7@gmail.com>
2026-05-23 23:21:38 +0530

    dp is added

--- 189minsumtriangulargrid.py
+++ 189minsumtriangulargrid.py
@@ -16,3 +16,28 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
         return self.recursion(0, 0, triangle, n)
+
+# using dp + recurison (TLE): TIme complexity is O(n*2)  where n is number in dp and each number has 2 states and space complexity is O(n) recurison stack+ dp (n^2)
+class Solution:
+    def recursion(self, i, j, triangle, n,dp):
+
+        if dp[i][j]!=-1:
+            return dp[i][j]
+        
+        if i == n - 1:
+            return triangle[i][j]
+        
+        down = self.recursion(i + 1, j, triangle, n,dp)
+
+        
+        diagonal = self.recursion(i + 1, j + 1, triangle, n,dp)
+
+        dp[i][j]= triangle[i][j] + min(down, diagonal)
+        return dp[i][j]
+
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+
+        # first pick row by row then after picking  read each row length puth that times -1
+        dp=[[-1]*len(row) for row in triangle]
+        return self.recursion(0, 0, triangle, n,dp)
