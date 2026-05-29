6d9fb63c217f0a45abe58cb9b08235b2598e612b
Solanki777 <solankimaheshkhash7@gmail.com>
2026-05-29 20:16:06 +0530

    cherry pickup 2

--- 191cherrypickup2.py
+++ 191cherrypickup2.py
@@ -65,4 +65,76 @@ class Solution:
         dp=[[[-1 for _ in range(col)] for _ in range(col)] for _ in range(row)]
  
         return self.recursion(grid,row,col,0,0,col-1,dp)
+
+# tabulation time complexity is O(row*col*col*9) and spac complexity is O(row*col*col)
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        row=len(grid)
+        col=len(grid[0])
+        dp=[[[-1 for _ in range(col)] for _ in range(col)] for _ in range(row)]
+
+        for i in range(col):
+            for j in range(col):
+                if i==j:
+                    dp[row-1][i][j]=grid[row-1][j]
+                else:
+                    dp[row-1][i][j]=grid[row-1][j]+grid[row-1][i]
+
+        for r in range(row-2,-1,-1):
+            for c1 in range(col):
+                for c2 in range(col):
+                    maxi=0
+                    for i in range(-1,2):
+                        for j in range(-1,2):
+                            new_c1=c1+i
+                            new_c2=c2+j
+
+                            if 0<=new_c1<col and 0<=new_c2<col:
+                                if c1==c2:
+                                    ans=grid[r][c1]
+                                else:
+                                    ans=grid[r][c1]+grid[r][c2]
+                                ans+=dp[r+1][new_c1][new_c2]
+                                maxi=max(maxi,ans)
+                    dp[r][c1][c2]=maxi
+        return dp[0][0][col-1]
+        
+# improved space complexity time complexity is O(row*col*col*9) and spac complexity is O(col*col)
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        row=len(grid)
+        col=len(grid[0])
+
+       
+        last=[[-1 for _ in range(col)] for _ in range(col)]
+        
+
+        for i in range(col):
+            for j in range(col):
+                if i==j:
+                    last[i][j]=grid[row-1][j]
+                else:
+                    last[i][j]=grid[row-1][j]+grid[row-1][i]
+
+        for r in range(row-2,-1,-1):
+            curr=[[-1 for _ in range(col)] for _ in range(col)]
+            for c1 in range(col):
+                for c2 in range(col):
+                    maxi=0
+                    for i in range(-1,2):
+                        for j in range(-1,2):
+                            new_c1=c1+i
+                            new_c2=c2+j
+
+                            if 0<=new_c1<col and 0<=new_c2<col:
+                                if c1==c2:
+                                    ans=grid[r][c1]
+                                else:
+                                    ans=grid[r][c1]+grid[r][c2]
+                                ans+=last[new_c1][new_c2]
+                                maxi=max(maxi,ans)
+                    curr[c1][c2]=maxi
+            last=curr
+        return last[0][col-1]
+        
         
