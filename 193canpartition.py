a24201be11b5369a361b2ccc15c7dad0fb66599e
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-01 23:05:10 +0530

    just simple recursion

--- 193canpartition.py
+++ 193canpartition.py
@@ -1,20 +1,21 @@
+# time complexity is O(2*n) and space complexity O(n)
 class Solution:
+    def recursion(self,nums,index,curr,s):
+        if curr==s:
+            return True
+        if curr>s or index>=len(nums):
+            return False
+        
+        inlcude=self.recursion(nums,index+1,curr+nums[index],s)
+        
+        not_inlcude=self.recursion(nums,index+1,curr,s)
+
+        return inlcude or not_inlcude
+
     def canPartition(self,nums):
-        s=[]
-        arr=[]
+        s=sum(nums)
+        if s%2!=0:
+            return False
+        s=s/2
+        return self.recursion(nums,0,0,s)
 
-        def recursion(nums,s,index,arr):
-            if index==len(nums):
-                s.append(arr.copy())
-                return
-            arr.append(nums[index])
-            recursion(nums,s,index+1,arr)
-            arr.pop()
-            recursion(nums,s,index+1,arr)
-
-        recursion(nums,s,0,arr)
-        return s
-
-arr=[5,3,2,1,0]
-sl=Solution()
-print(sl.canPartition(arr))
