fdb38f4e33e76050f13bc7f83f838a52c1b79c56
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-02 11:05:59 +0530

    recursion +dp

--- 194partitioninto2subsetwithminimaldiff.py
+++ 194partitioninto2subsetwithminimaldiff.py
@@ -11,5 +11,20 @@ class Solution:
     def minDifference(self, arr):
         return self.recursion(arr,0,0,0)
 
-
-		
+# using dp + recurison time complexity and space complexity is o(n*totalsum)
+class Solution:
+    def recursion(self,arr,s1,s2,index,dp):
+        if index>=len(arr):
+            return abs(s1-s2)
+        if dp[index][s1]!=-1:
+            return dp[index][s1]
+        
+        take1=self.recursion(arr,s1+arr[index],s2,index+1,dp)
+        take2=self.recursion(arr,s1,s2+arr[index],index+1,dp)
+        dp[index][s1]= min(take1,take2)
+        return dp[index][s1]
+    
+    def minDifference(self, arr):
+        total=sum(arr)
+        dp=[[-1 for _ in range(total+1)] for _ in range(len(arr))]
+        return self.recursion(arr,0,0,0,dp)
