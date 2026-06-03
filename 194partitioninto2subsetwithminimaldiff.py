23e01c1d895fb88c58b1fb7b4bf71365ecd6ed28
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-03 17:39:45 +0530

    min diff in subset of array

--- 194partitioninto2subsetwithminimaldiff.py
+++ 194partitioninto2subsetwithminimaldiff.py
@@ -28,3 +28,89 @@ class Solution:
         total=sum(arr)
         dp=[[-1 for _ in range(total+1)] for _ in range(len(arr))]
         return self.recursion(arr,0,0,0,dp)
+
+# tabulation time and space complexity is O(n*total)
+class Solution:
+    def minDifference(self,arr):
+        total=sum(arr)
+        dp=[[False for _ in range(total+1)] for _ in range(len(arr))]
+        
+
+        for i in range(len(arr)):
+            dp[i][0]=True
+        
+        dp[0][arr[0]]=True
+
+        for i in range(1,total+1):
+            for j in range(1,len(arr)):
+                take=False
+                not_take=dp[j-1][i]
+                if arr[j]<=i:
+                    take=dp[j-1][i-arr[j]]
+                dp[j][i]=take or not_take
+        
+        # we just require last row
+        min_diff=float('inf')
+        for s1 in range(total+1):
+            if dp[len(arr)-1][s1]==True:
+                s2=total-s1
+                curr_diff=abs(s2-s1)
+                min_diff=min(curr_diff,min_diff)
+        return min_diff
+
+# space complexity is improved o(total)
+class Solution:
+    def minDifference(self,arr):
+        total=sum(arr)
+        prev=[False for _ in range(total+1)]
+        
+
+        prev[0]=True
+        
+        prev[arr[0]]=True
+
+        for i in range(1,len(arr)):
+            curr=[False for _ in range(total+1)]
+            curr[0]=True
+            for j in range(1,total+1):
+                take=False
+                not_take=prev[j]
+                if arr[i]<=j:
+                    take=prev[j-arr[i]]
+                curr[j]=take or not_take
+            
+            
+
+            prev=curr
+        
+        # we just require last row
+        min_diff=float('inf')
+        for s1 in range(total+1):
+            if prev[s1]==True:
+                s2=total-s1
+                curr_diff=abs(s2-s1)
+                min_diff=min(curr_diff,min_diff)
+        return min_diff
+    
+# still it give tle because every time we creates new arr curr so to solve we have to done in one arr
+class Solution:
+    def minDifference(self,arr):
+        total=sum(arr)
+        dp=[False for _ in range(total+1)]
+        
+
+        dp[0]=True
+        
+
+        for num in arr:
+            for s in range(total,num-1,-1):
+                dp[s]=dp[s] or dp[s-num]     
+        
+        # we just require last row
+        min_diff=float('inf')
+        for s1 in range(total//2+1):
+            if dp[s1]==True:
+                s2=total-s1
+                curr_diff=abs(s2-s1)
+                min_diff=min(curr_diff,min_diff)
+        return min_diff
