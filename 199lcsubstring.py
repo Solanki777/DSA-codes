2dd8548838d3bd9c4aba800c332cb6d2b071e9b6
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-09 17:26:05 +0530

    recursion

--- 199lcsubstring.py
+++ 199lcsubstring.py
@@ -20,5 +20,21 @@ class Solution:
                 maxi=max(maxi,len(i))
         return maxi
 
+# let's use recursion time compelxity is O(3^(m+n)) and space complexity is (m+n)
+class Solution:
+    
+    def longCommSubstr(self, s1, s2):
+        def recursion(i,j,count):
+            if i==len(s1) or j==len(s2):
+                return count
+            
+            if s1[i]==s2[j]:
+                count= recursion(i+1,j+1,count+1)
+            return max(count,recursion(i+1,j,0),recursion(i,j+1,0))
+        return recursion(0,0,0)
+
+        
+
+
 
         
