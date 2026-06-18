4539f8f0134e9bc56874726edebc7b255d6e562c
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-18 09:32:14 +0530

    Count Distinct Substrings

--- 219countdistict.py
+++ 219countdistict.py
@@ -7,3 +7,28 @@ def countDistinctSubstrings(s):
         for j in range(i+1,len(s)+1):
             a.add(s[i:j])
     return len(a)+1
+
+# optimized space complexity 
+class TrieNode:
+    def __init__(self):
+        self.child={}
+           
+
+
+def countDistinctSubstrings(s):
+    root=TrieNode()
+    count=0
+
+    
+    for i in range(len(s)):
+        node=root
+        for j in range(i+1,len(s)+1):
+            char=s[i:j]
+            
+            if char not in node.child:
+                count+=1
+                node.child[char]=TrieNode()
+            node=node.child[char]            
+
+    return count+1
+            
