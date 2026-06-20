115ee2bb7e4e11ca7ad82e3be69878216bd6730c
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-20 23:01:05 +0530

    maxBuilding

--- Greedy/1.Maximum Building Height.py
+++ Greedy/1.Maximum Building Height.py
@@ -22,4 +22,33 @@ class Solution:
 
         return ans
 
+# instead of traversing all values now we traveerse only restrected values and calulate the same way as above
+
+# time complexity is O(m log m) and space complexity is O(1)
+class Solution:
+    def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
+        restrictions.append([1,0])
+        if not any(idx == n for idx, _ in restrictions):
+            restrictions.append([n, n - 1])
+        restrictions.sort()
+
+        for  i in range(1,len(restrictions)):
+
+            diff=restrictions[i][0]-restrictions[i-1][0]
+            restrictions[i][1]=min(restrictions[i][1],restrictions[i-1][1]+diff)
+        
+        for i in range(len(restrictions)-2,-1,-1):
+            diff=restrictions[i+1][0]-restrictions[i][0]
+            restrictions[i][1]=min(restrictions[i][1],restrictions[i+1][1]+diff)
+
+        ans=0
+        for i in range(1,len(restrictions)):
+            x1,h1=restrictions[i-1]
+            x2,h2=restrictions[i]
+            
+            dist=x2-x1
+            ans=max(ans,(h1+h2+dist)//2)
+        return ans
+            
+
         
