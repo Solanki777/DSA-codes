af5521bde67a245ae6a7eccfbcda0960ca79d986
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-21 10:06:35 +0530

    maximum ice cream bars

--- Dynamic Programming/1.maximam_ice_crea_bars.py
+++ Dynamic Programming/1.maximam_ice_crea_bars.py
@@ -1,4 +1,4 @@
-# brute force solution time complexity is O(nlogn) and space complexity is O(n)
+# brute force solution time complexity is O(nlogn) and space complexity is O(n) which max solution for this 
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
         costs.sort()
@@ -48,6 +48,70 @@ class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
         dp=[[-1 for _ in range(coins+1)] for _ in range(len(costs))]
         return self.recurison(costs,0,coins,dp)
+
+# tabulation : time and space complexity is O(coins*len(costs))
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        dp=[[0 for _ in range(coins+1)] for _ in range(len(costs))]
+
+        for coin in range(coins+1):
+            if coin>=costs[0]:
+                dp[0][coin]=1
+        
+        for index in range(1,len(costs)):
+            for coin in range(1,coins+1):
+
+                buy=0
+                if coin>=costs[index]:
+                    buy=1+dp[index-1][coin-costs[index]]
+                not_buy=dp[index-1][coin]
+                dp[index][coin]=max(buy,not_buy)
+
+        return dp[len(costs)-1][coins]
+
+# space optimization
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        prev=[0 for _ in range(coins+1)] 
+
+        for coin in range(coins+1):
+            if coin>=costs[0]:
+                prev[coin]=1
+        
+        for index in range(1,len(costs)):
+            curr=[0 for _ in range(coins+1)]
+            for coin in range(1,coins+1):
+
+                buy=0
+                if coin>=costs[index]:
+                    buy=1+prev[coin-costs[index]]
+                not_buy=prev[coin]
+                curr[coin]=max(buy,not_buy)
+            prev=curr
+
+        return prev[coins]
+    
+# further space optimization
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        curr=[0 for _ in range(coins+1)] 
+
+        for coin in range(coins+1):
+            if coin>=costs[0]:
+                curr[coin]=1
+        
+        for index in range(1,len(costs)):
+            for coin in range(coins,-1,-1):
+
+                buy=0
+                if coin>=costs[index]:
+                    buy=1+curr[coin-costs[index]]
+                not_buy=curr[coin]
+                curr[coin]=max(buy,not_buy)
+
+        return curr[coins]
+
+        
 
         
 
