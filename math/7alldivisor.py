87742f4707ebb7a7fa2855c699b66493cd19355b
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-22 11:33:44 +0530

    all divisors

---
+++ math/7alldivisor.py
@@ -0,0 +1,25 @@
+# time complexity is O(n) and space complexity is O(divisor)
+class Solution:
+    def getDivisors(self, n):
+        ans=[]
+        for i in range(1,n+1):
+            if n%i==0:
+                ans.append(i)
+        return ans
+    
+from math import sqrt
+class Solution:
+    def getDivisors(self, n):
+        ans=[]
+        for i in range(1,int(sqrt(n))+1):
+            if n%i==0:
+                ans.append(i)
+                if n//i != i :
+                    ans.append(n//i)
+        ans.sort()
+        return ans
+    
+# time complexity is O(logn) and space complexity is O(1)
+    
+
+        
