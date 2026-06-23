379994ac0d0836bf2c6489faaf4639981e46eb4c
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-23 21:46:17 +0530

    recursion and dp

---
+++ Dynamic Programming/2.number_of_zigzag_arr1.py
@@ -0,0 +1,58 @@
+class Solution:
+    def recursion(self,prev2,prev1,index,n,l,r):
+        if index==n:
+            return 1
+
+        ans=0
+
+        for i in range(l,r+1):
+            if prev1!=-1 and i==prev1:
+                continue
+
+            if prev2!=-1 and prev1!=-1:
+                
+                if prev2<prev1<i:
+                    continue
+                if prev2>prev1>i:
+                    continue
+
+            ans+=self.recursion(prev1,i,index+1,n,l,r)
+
+        return ans
+
+    def zigZagArrays(self,n:int,l:int,r:int)->int:
+        return self.recursion(-1,-1,0,n,l,r)
+
+# dp + recursion solution
+class Solution:
+    def recursion(self,prev2,prev1,index,n,l,r,dp):
+        if index==n:
+            return 1
+
+        key=(prev2,prev1,index)
+        
+        if key in dp:
+            return dp[key]
+
+        ans=0
+
+        for i in range(l,r+1):
+            if prev1!=-1 and i==prev1:
+                continue
+
+            if prev2!=-1 and prev1!=-1:
+                
+                if prev2<prev1<i:
+                    continue
+                if prev2>prev1>i:
+                    continue
+
+            ans+=self.recursion(prev1,i,index+1,n,l,r,dp)
+        dp[key]=ans % (10**9 + 7)
+
+        return dp[key]
+
+    def zigZagArrays(self,n:int,l:int,r:int)->int:
+        dp={}
+        return self.recursion(-1,-1,0,n,l,r,dp)
+
