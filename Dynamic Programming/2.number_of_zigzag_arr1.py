8254400a4e30697ce0105152704b73b580e36b15
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-23 23:27:55 +0530

    number of zigzag array 1

--- Dynamic Programming/2.number_of_zigzag_arr1.py
+++ Dynamic Programming/2.number_of_zigzag_arr1.py
@@ -58,4 +58,29 @@ class Solution:
         return self.recursion(-1,-1,0,n,l,r,dp)
 
 
+# time and space complexity is O(nm)
+class Solution:
+    def zigZagArrays(self, n: int, l: int, r: int) -> int:
+        MOD=10**9 + 7
+        r-=l
+        dp=[[0] *(r+1) for _ in range(n)]
 
+        for j in range(r+1):
+            dp[0][j]=1
+        
+        for i in range(1,n):
+            prev=0
+            if i%2==1:
+                for j in range(r+1):
+                    dp[i][j]=prev
+                    prev=(prev+dp[i-1][j])%MOD
+            else:
+                for j in range(r,-1,-1):
+                    dp[i][j]=prev
+                    prev=(prev+dp[i-1][j])%MOD
+        
+        return sum(dp[-1]*2)%MOD
+
+
+
+        
