5df80bd704403a00e6a4c4eff7a4d6d6e3533082
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-25 21:31:24 +0530

    zig zag 1

--- prefix_sum/2.zig_zag_arr_1.py
+++ prefix_sum/2.zig_zag_arr_1.py
@@ -2,6 +2,8 @@
 class Solution:
     def zigZagArrays(self, n: int, l: int, r: int) -> int:
         MOD=10**9 + 7
+
+        # m 
         r=r-l+1
         dp=[[0] *(r) for _ in range(n)]
 
@@ -25,6 +27,36 @@ class Solution:
         
         return (sum(dp[n-1])*2)%MOD
 
+# Time complexity and space complexity  is O(n*m) 
+class Solution:
+    def zigZagArrays(self, n: int, l: int, r: int) -> int:
+        MOD=10**9 + 7
+
+        # m 
+        r=r-l+1
+        dp=[[0] *(r) for _ in range(n)]
+
+        for j in range(r):
+            dp[0][j]=1
+        
+        for i in range(1,n):
+            prev=0
+
+            if i%2==0:
+                for j in range(r):
+                    dp[i][j]=prev
+                    prev = (dp[i][j] + dp[i-1][j]) % MOD
+            else:
+                for j in range(r-1,-1,-1):
+                    dp[i][j]=prev
+                    prev=(dp[i][j]+dp[i-1][j]) %  MOD
+
+        return (sum(dp[n-1])*2)%MOD
+
+
+
+
+        
 
 
 
