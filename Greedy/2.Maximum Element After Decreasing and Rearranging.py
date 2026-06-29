a86e5eb8749a22adfc7c16cb2571f062f9f195a7
Solanki777 <solankimaheshkhash7@gmail.com>
2026-06-29 12:52:24 +0530

    Maximum Element After Decreasing and Rearranging

---
+++ Greedy/2.Maximum Element After Decreasing and Rearranging.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+
+        if arr[0]!=1:
+            arr[0]=1
+
+        for i in range(1,len(arr)):
+            if arr[i]-arr[i-1]>1:
+                arr[i]=arr[i-1]+1
+        
+        return arr[-1]
+                
+
+# Time complexity is O(len(arr)) and space complexity is O(1)
+
+            
+
+        
