64093c405aeab90d913fe884037e94b5a5b5bd71
Solanki777 <solankimaheshkhash7@gmail.com>
2026-07-06 15:58:09 +0530

    Number of Paths with Max Score

--- 17 .Dynamic Programming/Number of Paths with Max Score.py
+++ 17 .Dynamic Programming/Number of Paths with Max Score.py
@@ -33,3 +33,52 @@ class Solution:
             return [0,0]
         
         return [self.max_score,self.paths%MOD]
+    
+# time and space complexity is O(n*n)
+class Solution:
+    def pathsWithMaxScore(self, board: List[str]) -> List[int]:
+        n = len(board)
+        dp=[[[-1,0] for _ in range(n)] for _ in range(n)]
+        MOD= 10 ** 9 +7
+
+        for i in range(n-1,-1,-1):
+            for j in range(n-1,-1,-1):
+
+                if board[i][j]=='X':continue
+                if board[i][j]=='S':
+                    dp[i][j][0]=0
+                    dp[i][j][1]=1
+                    continue
+    
+                if board[i][j]=='E':
+                    num=0
+                else:
+                    num=int(board[i][j])
+                
+                best=-1
+                path=0
+
+                for di,dj in ((i+1,j) , (i+1,j+1) , (i,j+1)):
+                     
+                    if di>=n or dj >=n : continue
+                    if dp[di][dj][0]==-1:continue
+                    
+                    
+                    score,count=dp[di][dj]
+
+                    if score>best:
+                        best=score
+                        path=count
+
+                    elif score==best:
+                        path= (path + count) % MOD
+                
+                if best!=-1:
+                    dp[i][j]=[best + num , path]
+        
+        return dp[0][0] if dp[0][0][0] != -1 else [0,0]
+
+    
+
+
+        
