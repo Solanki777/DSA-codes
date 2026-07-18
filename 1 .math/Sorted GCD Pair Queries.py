3397e107e6bf0145b0c8c4cf185b0cce156b3009
Solanki777 <solankimaheshkhash7@gmail.com>
2026-07-18 17:01:37 +0530

    Sorted GCD Pair Queries

--- 1 .math/Sorted GCD Pair Queries.py
+++ 1 .math/Sorted GCD Pair Queries.py
@@ -28,6 +28,66 @@ class Solution:
         return res
 
 # Time complexity is O(n^2logn) and space complexity is O(n^2) for pairs +O(q) answers
+
+
+# Let:
+
+# n=len(nums)
+# q=len(queries)
+# m=max(nums)
+
+class Solution:
+    def binarysearch( self, i ,j ,nums , el):
+        ans = 0
+        while i <= j:
+            mid = i + (j - i ) //2
+
+            if nums[mid] > el :
+                ans = mid
+                j = mid -1
+            else:
+                
+                i = mid + 1
+                
+
+        return ans
+
+    def gcdValues(self, nums: List[int], queries: List[int]) -> List[int]:
+        m = max(nums)
+        facto = [0] *(m+1)
+        
+        # O(n) 
+        for x in nums:
+            facto[x] += 1
+
+        # get all divisivles O(m log m)
+        for i in range(1,m+1):
+            for j in range(i*2,m+1,i):
+                facto[i] += facto[j]
+
+        # getting pairs O(m)
+        for i in range(1,m+1):
+            pairs = int (( facto[i] * (facto[i] -1)) // 2)
+            facto[i] = pairs
+        
+        # remove duplicates O(m log m)
+        for i in range(m,0,-1):
+            for j in range(i*2 , m +1 , i ):
+                facto[i] -= facto[j]
+        
+        #cummulative sum O(m)
+        for i in range(1,m+1):
+            facto[i] += facto [i-1]
+
+        # binary search O(q log m)
+        res = []
+        for q in queries:
+            t = self.binarysearch(0,m+1,facto,q)
+            res.append(t)
+        
+        return res
+
+# Time complexity is O(q + mlog m + q log m) and space complexity is O(m)
         
 
 
