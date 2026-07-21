50e13ea5f1df4ad94cf9db943345002257b905ba
Solanki777 <solankimaheshkhash7@gmail.com>
2026-07-21 17:39:44 +0530

    class decorator

--- OOP Prac by Apna College/super.py
+++ OOP Prac by Apna College/super.py
@@ -17,3 +17,29 @@ print(s1.college_name)
 
 
 
+# How to access class varibales
+
+# 1. normal way 
+class Person:
+    name = "annonymous"
+    def __init__(self,name):
+        # 1.# Person.name = name
+        #2
+        self.__class__.name = name
+
+
+print(Person.name)
+Person("John")
+print(Person.name)
+
+
+# using class decorator
+class Person:
+    name = "annonymous"
+
+    @classmethod
+    def __init__(cls,name):
+        cls.name = name
+
+Person("Lanna")
+print(Person.name)        
