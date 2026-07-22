62ba90e4d29cb71bb6dfe8ed6f44a7c417be55ba
Solanki777 <solankimaheshkhash7@gmail.com>
2026-07-22 18:30:27 +0530

    polymorphism

--- OOP Prac by Apna College/polymorphism.py
+++ OOP Prac by Apna College/polymorphism.py
@@ -31,3 +31,27 @@
 # c2.show()
 # c3 = c2.add(c1)
 # c3.show()
+
+# but writing function is not sufficient i want to make like just do 
+# c3 = c1 + c2
+class Complex :
+    def __init__(self , real, img):
+        self.real = real
+        self.img = img
+    
+    def show(self):
+        print(self.real ,"i +" , self.img,"j" )
+    
+    def __add__(self,n2):
+        self.new_real = self.real + n2.real
+        self.new_img = self.img + n2.img
+        c3 = Complex(self.new_real , self.new_img)
+        return c3
+
+c1 = Complex(6,4)
+c1.show()
+c2 = Complex(2,3)
+c2.show()
+# c3 = c1 + c2 it gives error now we use dunder function 
+c3 = c1 + c2
+print(c3.show())
